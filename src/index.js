902b5e6b88ea5b88c94d652353f690832f25d742
Noblesse4Razor <noblesse4razor@gmail.com>
2017-02-22 00:00:30 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ module.exports = function multiply(first, second) {
     for (var i = 0; i < first.length; i++) {
         for (var j = 0; j < second.length; j++) {
 
-            var Index = i + j;
-            if (!result[Index]) result[Index] = 0;
 
-            result[Index] += first[i] * second[j] ;
+            if (!result[i+j]) result[i+j] = 0;
+
+            result[i+j] += first[i] * second[j] ;
 
             for (var k = 0; k < result.length; k++) {
                 if (result[k] >= 10) {
