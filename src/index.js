cc3623543aa7fa1a9dd805360474e5da84417041
Noblesse4Razor <noblesse4razor@gmail.com>
2017-02-21 23:37:32 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -3,25 +3,25 @@
 module.exports = function multiply(first, second) {
     var a1 = first.split("").reverse();
     var a2 = second.toString().split("").reverse();
-    var aResult = [];
+    var result = [];
 
-    for (var iterNum1 = 0; iterNum1 < a1.length; iterNum1++) {
-        for (var iterNum2 = 0; iterNum2 < a2.length; iterNum2++) {
+    for (var i = 0; i < a1.length; i++) {
+        for (var j = 0; j < a2.length; j++) {
 
-            var idxIter = iterNum1 + iterNum2;
+            var idxIter = i + j;
             var tt = 0;
-            if (idxIter >= aResult.length) tt = 0;else tt = aResult[idxIter];
-            aResult[idxIter] = a1[iterNum1] * a2[iterNum2] + (idxIter >= aResult.length ? 0 : aResult[idxIter]);
+            if (idxIter >= result.length) tt = 0;else tt = result[idxIter];
+            result[idxIter] = a1[i] * a2[j] + (idxIter >= result.length ? 0 : result[idxIter]);
 
-            if (aResult[idxIter] > 9) {
+            if (result[idxIter] > 9) {
                 var t = 0;
-                if (idxIter + 1 >= aResult.length) t = 0;else {
-                    t = aResult[idxIter + 1];
+                if (idxIter + 1 >= result.length) t = 0;else {
+                    t = result[idxIter + 1];
                 }
-                aResult[idxIter + 1] = ((aResult[idxIter] / 10)| 0) + t;
-                aResult[idxIter] -= ((aResult[idxIter] / 10)| 0) * 10;
+                result[idxIter + 1] = ((result[idxIter] / 10)| 0) + t;
+                result[idxIter] -= ((result[idxIter] / 10)| 0) * 10;
             }
         }
     }
-    return (aResult.reverse().join(""));
+    return (result.reverse().join(""));
 };
