01ed0baef4b02d199c2e45361a4c0510e3c17012
Noblesse4Razor <noblesse4razor@gmail.com>
2017-02-21 23:56:12 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 module.exports = function multiply(first, second) {
-    var a1 = first.split('').reverse();
-    var a2 = second.split('').reverse();
+     first = first.split('').reverse();
+     second = second.split('').reverse();
     var result = [];
 
-    for (var i = 0; i < a1.length; i++) {
-        for (var j = 0; j < a2.length; j++) {
+    for (var i = 0; i < first.length; i++) {
+        for (var j = 0; j < second.length; j++) {
 
             var Index = i + j;
             if (!result[Index]) result[Index] = 0;
 
-            result[Index] += a1[i] * a2[j] ;
+            result[Index] += first[i] * second[j] ;
 
             for (var k = 0; k < result.length; k++) {
                 if (result[k] >= 10) {
